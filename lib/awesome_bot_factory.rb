e210415459d3592886603fe79e0b9238dd5559a0
Michael Bumann <michael@railslove.com>
2011-09-07 02:31:25 +0200

    add example payload class for easier testing

--- lib/awesome_bot_factory.rb
+++ lib/awesome_bot_factory.rb
@@ -1,7 +1,9 @@
 require "json"
 require "awesome_bot_factory/version"
 require 'active_support/configurable'
+require 'active_support/core_ext/hash/keys'
 
 module AwesomeBotFactory
   autoload :Skill, 'awesome_bot_factory/skill'
+  autoload :ExamplePayload, 'awesome_bot_factory/example_payload'
 end
---
+++ lib/awesome_bot_factory/example_payload.rb
@@ -0,0 +1,32 @@
+module AwesomeBotFactory
+  class ExamplePayload < Hash
+    DEFAULTS = {
+      "body" => "campfire message",
+      "room_id" =>  "123456",
+      "user_id" => "7890",
+      "user" => {
+        "name" => "Choco Bit", 
+        "avatar_url" => "http://asset0.37img.com/global/.../avatar.png", 
+        "type" => "Member", 
+        "admin" => false
+      },
+      "user_name" => "choco",
+      "match" => ["key:command value", "command", "value"],
+      "slug" => "hexhash"
+    }
+    
+    def initialize(attributes={})
+      self.merge!(DEFAULTS)
+      self.merge!(attributes)
+    end
+    
+    def as_hash
+      self
+    end
+    
+    def as_json
+      as_hash.to_json
+    end
+    
+  end
+end
