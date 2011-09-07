b2fe8ea98f6524e5cb2c2a02c99db085ccb59e91
Michael Bumann <michael@railslove.com>
2011-09-07 02:32:35 +0200

    simplify skill class and add read_attribute method this allows overwriting of matches method and still access the original value

--- lib/awesome_bot_factory/skill.rb
+++ lib/awesome_bot_factory/skill.rb
@@ -5,11 +5,7 @@ module AwesomeBotFactory
     attr_accessor :message
     
     def self.matches(*matches)
-      matches.each_with_index do |name,index|
-        define_method name do 
-          self.message["match"][index+1]
-        end
-      end
+      @@matches = matches
     end
     
     def self.call(env)
@@ -17,13 +13,21 @@ module AwesomeBotFactory
       [200, {"Content-Type" => "application/json"}, [ self.new(message).reply.to_json] ]
     end
   
-    def initialize(campfire_message)
+    def initialize(campfire_message={})
       self.message = campfire_message
+      self.message["match"] ||= {} # let's just make sure we have an hash there and don't break if it is in any case empty
     end
-  
+    
+    def read_attribute(attr)
+      index = @@matches.index(attr.to_sym)
+      return nil unless index
+      self.message["match"][index+1]
+    end
+    
     def method_missing(name, *args)
       return self.message[name.to_s] if self.message.key?(name.to_s)
       return self.message[name.to_sym] if self.message.key?(name.to_sym)
+      return self.read_attribute(name) if self.read_attribute(name)
       super(name,*args)
     end
   end
