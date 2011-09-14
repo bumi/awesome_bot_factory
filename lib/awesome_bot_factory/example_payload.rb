cf15a2a002a6e4d56d41c0cc3c6cbf284260936e
Michael Bumann <michael@railslove.com>
2011-09-14 11:03:14 +0200

    misc formatting

--- lib/awesome_bot_factory/example_payload.rb
+++ lib/awesome_bot_factory/example_payload.rb
@@ -5,28 +5,28 @@ module AwesomeBotFactory
       "room_id" =>  "123456",
       "user_id" => "7890",
       "user" => {
-        "name" => "Choco Bit", 
-        "avatar_url" => "http://asset0.37img.com/global/.../avatar.png", 
-        "type" => "Member", 
+        "name" => "Choco Bit",
+        "avatar_url" => "http://asset0.37img.com/global/.../avatar.png",
+        "type" => "Member",
         "admin" => false
       },
       "user_name" => "choco",
       "match" => ["key:command value", "command", "value"],
       "slug" => "hexhash"
     }
-    
+
     def initialize(attributes={})
       self.merge!(DEFAULTS)
       self.merge!(attributes)
     end
-    
+
     def as_hash
       self
     end
-    
+
     def as_json
       as_hash.to_json
     end
-    
+
   end
 end
--- lib/awesome_bot_factory/skill.rb
+++ lib/awesome_bot_factory/skill.rb
@@ -1,23 +1,23 @@
 module AwesomeBotFactory
   class Skill
     include ActiveSupport::Configurable
-    
+
     attr_accessor :message
-    
+
     def self.matches(*matches)
       @@matches = matches
     end
-    
+
     def self.call(env)
       message = JSON.parse(env["rack.input"].read)
       [200, {"Content-Type" => "application/json"}, [ self.new(message).reply.to_json] ]
     end
-  
+
     def initialize(campfire_message={})
       self.message = campfire_message
       self.message["match"] ||= [] # let's just make sure we have an hash there and don't break if it is in any case empty
     end
-    
+
     def read_attribute(attr)
       index = @@matches.index(attr.to_sym)
       if index
@@ -26,11 +26,11 @@ module AwesomeBotFactory
         self.message[attr.to_s] || self.message[attr.to_sym]
       end
     end
-    
+
     def attribute_defined?(attr)
       !@@matches.index(attr).nil?
     end
-    
+
     def method_missing(name, *args)
       return self.message[name.to_s] if self.message.key?(name.to_s)
       return self.message[name.to_sym] if self.message.key?(name.to_sym)
